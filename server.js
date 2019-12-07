9a5d4cd7471b45b57b7c6cde5b986da707802f60
Nitin Gupta <gupta7nitin@gmail.com>
2019-12-07 22:46:32 +0530

    Upload Files BackEnd

--- server.js
+++ server.js
@@ -91,6 +91,27 @@ app.post('/signin',function(req,res){
       res.send("0");
 });
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+app.post('/uploadFile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(file)
+})
+
 app.listen(3000,function()					//Server Running Confirmation
 {
       console.log("Running on port 3000");
