c961ab1be9d0810401393551aab9b63b5f4f8afd
Nitin Gupta <gupta7nitin@gmail.com>
2019-12-07 16:29:06 +0530

    server done

---
+++ server.js
@@ -0,0 +1,93 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+var session = require('express-session')
+var nodemailer = require('nodemailer');
+var multer = require('multer');
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public/uploads')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(session({
+  secret: "LifeIsSad",
+  resave: false,
+  saveUnintialized: true,
+}))
+
+app.use(function (req, res, next) {
+  next();
+})
+
+var mongoose = require('mongoose');
+var userdb = 'mongodb://localhost/quickStay';
+
+mongoose.connect(userdb);
+
+mongoose.connection.on('error',(err) => {
+  console.log('DB connection Error');
+})
+
+mongoose.connection.on('connected',(err) => {
+   useNewUrlParser: true;
+  console.log('DB connected');
+})
+
+var userSchema = new mongoose.Schema({
+    fullname: String,
+    phone: String,
+    email: String,
+    password: String,
+})
+
+var user = mongoose.model('users', userSchema);
+
+app.get('/adduser',logger,function(req,res)
+{
+    res.render('adduser',{obj : req.session.data , exists:exists});
+    exists=0;
+})
+
+app.post('/adduser',function(req,res)
+{
+    exists = 0;
+    var obj = req.body;
+    user.find({
+      username: req.body.username,
+    })
+    .then(data =>
+    {
+        if(data.length != 0){
+          exists = 1;
+          res.redirect('/adduser')
+        }
+        else{
+            console.log(obj)
+            user.create(obj,function(error,result){
+                if(error)
+                throw err;
+            })
+            res.render('profile',{obj : req.session.data});
+        }
+    });
+});
+
+app.listen(3000,function()					//Server Running Confirmation
+{
+      console.log("Running on port 3000");
+});
+
+//MIDDLEWARE FUNCTIONS
+function logger(req,res,next)				//login vaala 
+{
+  if(req.session.isLogin)
+  {
+    next();
+  }
+  else {
+      res.redirect('/');
+  }
+}
